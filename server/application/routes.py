3bd2ad62f08603de5e6ed1f39d251411a7669bc1
htr-volker <htr.volker@gmail.com>
2020-01-09 09:11:54 +0000

    syncing

--- server/application/routes.py
+++ server/application/routes.py
@@ -9,29 +9,30 @@ import requests
 # Route to home page
 @app.route("/server/home", methods = ["GET", "POST"])
 def home():
-	app.logger.info("******************************************************************************")
-	# button to generate a random number
-	generate_band = GenerateBandButton()
+    #app.logger.info("******************************************************************************")
+    # button to generate a random number
+    generate_band = GenerateBandButton()
 	
 
-	if request.method == "POST":
-		band = request.get_json()
-		app.logger.info(f"Package received \n Contents: {band}")
-	else:
-		band = {"name" : "",
-			"genre" : "",
-			"number of members" : "",
-			"popularity" : "",
-			"pretentiousness" : "",
-			"members" : [""]}
+    if request.method == "POST":
+        band = request.get_json()
+        app.logger.info(f"Package received \n Contents: {band}")
+    else:
+        band = {"name" : "",
+		"genre" : "",
+		"number of members" : "",
+		"popularity" : "",
+		"pretentiousness" : "",
+		"members" : [""]}
 
-	# send request to name_generator to begin generating when button is pressed
-	if generate_band.is_submitted():
-		requests.get("http://name_generator:5002/service2")
-		app.logger.info(f"Package requested")
+    # send request to name_generator to begin generating when button is pressed
+    if generate_band.is_submitted():
+        app.logger.info("********************************************")
+        requests.get("http://name_generator:5002/service2")
+        app.logger.info(f"Package requested")
 
-	return render_template("index.html", title = "Home", band = band, generate_band = generate_band)
+    return render_template("index.html", title = "Home", band = band, generate_band = generate_band)
 
-@app.route("/server/health-check", methods=["GET"])
-def health_check():
+    @app.route("/server/health-check", methods=["GET"])
+    def health_check():
         return "OK"
