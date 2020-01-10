699595e1ce16c307a8ea590da1f4a7c0999fa92b
htr-volker <htr.volker@gmail.com>
2020-01-10 17:31:44 +0000

    fix import issue with forms.py in server

--- server/application/forms.py
+++ server/application/forms.py
@@ -3,9 +3,7 @@ Provides web forms built using WTForms.
 Basic user sign up form has been provided for reference.
 '''
 from flask_wtf import FlaskForm
-from wtforms import StringField, DateTimeField, FloatField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
-from application import db, bcrypt
+from wtforms import SubmitField
 
 class GenerateBandButton(FlaskForm):
     submit = SubmitField('Generate Band')
