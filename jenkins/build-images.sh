0416ba657fa366f3187d50ec83828d4558906293
htr-volker <htr.volker@gmail.com>
2020-01-11 22:05:31 +0000

    pipeline fix 2

--- jenkins/build-images.sh
+++ jenkins/build-images.sh
@@ -1,4 +1,4 @@
 # Build the images and push them to the local docker registry
-cp /home/jenkins/.env /var/lib/jenkins/workspace/deploy-services/.env
+cp /home/jenkins/.env /var/lib/jenkins/workspace/staging/.env
 docker-compose build
 docker-compose push
