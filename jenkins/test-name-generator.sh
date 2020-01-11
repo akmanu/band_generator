9174e6144879e9e5fda4bca755ab95a6424c6451
htr-volker <htr.volker@gmail.com>
2020-01-11 22:35:06 +0000

    pipeline fix 7

--- jenkins/test-name-generator.sh
+++ jenkins/test-name-generator.sh
@@ -1,6 +1,6 @@
 # test name generator (service 2)
 pip3 install -r 2_name_generator/requirements.txt
-python3 -m pytest --cov=application . --cov-report html:cov_html
+python3 -m pytest --cov=application 2_name_generator/ --cov-report html:cov_html
 ssh jenkins@bg-manager-node "mkdir -p /home/jenkins/test-reports/name_generator/"
 scp cov_html/index.html jenkins@bg-manager-node:/home/jenkins/test-reports/name_generator/coverage.html
 scp cov_html/index.html jenkins@bg-manager-node:/home/jenkins/test-reports/name_generator/$BULID_ID.html
--- jenkins/test-stats-generator.sh
+++ jenkins/test-stats-generator.sh
@@ -1,6 +1,6 @@
 # test stats generator (service 3)
 pip3 install -r 3_stats_generator/requirements.txt
-python3 -m pytest --cov=application . --cov-report html:cov_html
+python3 -m pytest --cov=application 3_stats_generator/ --cov-report html:cov_html
 ssh jenkins@bg-manager-node "mkdir -p /home/jenkins/test-reports/stats_generator/"
 scp cov_html/index.html jenkins@bg-manager-node:/home/jenkins/test-reports/stats_generator/coverage.html
 scp cov_html/index.html jenkins@bg-manager-node:/home/jenkins/test-reports/stats_generator/$BULID_ID.html
