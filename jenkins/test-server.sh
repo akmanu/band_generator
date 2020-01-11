45220f9e92ef2b45b0174339c39365ec542d32b0
htr-volker <htr.volker@gmail.com>
2020-01-11 23:18:56 +0000

    pipeline fix 12

--- jenkins/test-server.sh
+++ jenkins/test-server.sh
@@ -3,5 +3,5 @@ pip3 install -r 1_server/requirements.txt
 python3 -m pytest --cov=application 1_server/ --cov-report html:cov_html
 ssh jenkins@bg-manager-node "mkdir -p /home/jenkins/test-reports/server/"
 scp cov_html/index.html jenkins@bg-manager-node:/home/jenkins/test-reports/server/coverage.html
-scp cov_html/index.html jenkins@bg-manager-node:/home/jenkins/test-reports/server/$BULID_ID.html
+scp cov_html/index.html jenkins@bg-manager-node:/home/jenkins/test-reports/server/$BUILD_ID.html
 rm -r cov_html
