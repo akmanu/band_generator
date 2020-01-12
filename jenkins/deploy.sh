46d1a5d9e2498f42083834462a72e839d28698a9
htr-volker <htr.volker@gmail.com>
2020-01-12 16:53:37 +0000

    build id fix 3

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -2,5 +2,4 @@
 scp docker-compose.yaml jenkins@bg-manager-node:/home/jenkins/
 
 # Deploy on the manager node
-ssh bg-manager-node "export BUILD_ID=$BUILD_ID"
-ssh bg-manager-node "docker stack deploy --compose-file docker-compose.yaml band_generator"
+ssh bg-manager-node "export BUILD_ID=$BUILD_ID ; docker stack deploy --compose-file docker-compose.yaml band_generator"
