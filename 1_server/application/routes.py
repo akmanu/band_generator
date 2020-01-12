86cd412ce1edca8a02d4f755ba4d4e48de0bc737
htr-volker <htr.volker@gmail.com>
2020-01-12 17:05:25 +0000

    as before, take 3

--- 1_server/application/routes.py
+++ 1_server/application/routes.py
@@ -28,7 +28,7 @@ def home():
 		band = response.json()
 		app.logger.info(f"Package requested")
 
-	return render_template("index.html", title = "Home", band = band, generate_band = generate_band)
+	return render_template("index.html", title = "Home", band = band, generate_band = generate_band, build_id = build_id)
 
 @app.route("/home/health-check", methods=["GET"])
 def health_check():
