ec79953bae75b02fc071f702024cf62c138a1945
htr-volker <htr.volker@gmail.com>
2020-01-12 01:07:38 +0000

    pipeline fix 17

--- jenkins/test-members-generator.sh
+++ jenkins/test-members-generator.sh
@@ -1,7 +1,7 @@
 # test members generator (service 4)
 export $(xargs <.env)
 pip3 install -r 4_members_generator/requirements.txt
-python3.7 -m pytest --cov=application 4_members_generator/ --cov-report html:cov_html
+python3 -m pytest --cov=application 4_members_generator/ --cov-report html:cov_html
 ssh jenkins@bg-manager-node "mkdir -p /home/jenkins/test-reports/members_generator/"
 scp cov_html/index.html jenkins@bg-manager-node:/home/jenkins/test-reports/members_generator/coverage.html
 scp cov_html/index.html jenkins@bg-manager-node:/home/jenkins/test-reports/members_generator/$BUILD_ID.html
--- jenkins/test-server.sh
+++ jenkins/test-server.sh
@@ -1,7 +1,7 @@
 # test server (service 1)
 export $(xargs <.env)
 pip3 install -r 1_server/requirements.txt
-python3.7 -m pytest --cov=application 1_server/ --cov-report html:cov_html
+python3 -m pytest --cov=application 1_server/ --cov-report html:cov_html
 ssh jenkins@bg-manager-node "mkdir -p /home/jenkins/test-reports/server/"
 scp cov_html/index.html jenkins@bg-manager-node:/home/jenkins/test-reports/server/coverage.html
 scp cov_html/index.html jenkins@bg-manager-node:/home/jenkins/test-reports/server/${BUILD_ID}.html
