ad18cbcc1b3de382c5677b4395f979ce55500b0e
htr-volker <htr.volker@gmail.com>
2020-01-12 02:16:30 +0000

    pipeline fix 27

--- 1_server/application/__init__.py
+++ 1_server/application/__init__.py
@@ -3,6 +3,7 @@ Main flask file to be run to start the app
 Requires environment variables to be set in order to run
 '''
 from flask import Flask, render_template
+from os import getenv
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = getenv('MYSQL_SECRETKEY')
