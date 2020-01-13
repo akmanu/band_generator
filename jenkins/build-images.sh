b5419423bdf84f9c4618a039c1dc30689f37186f
htr-volker <htr.volker@gmail.com>
2020-01-13 01:56:45 +0000

    build-images.sh fix

--- jenkins/build-images.sh
+++ jenkins/build-images.sh
@@ -1,4 +1,4 @@
 # Build the images and push them to the local docker registry
-cp /home/jenkins/.env /var/lib/jenkins/workspace/implemenation2/.env
+cp /home/jenkins/.env /var/lib/jenkins/workspace/implementation2/.env
 docker-compose build
 docker-compose push
